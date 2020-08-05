472aa7324e24adc1c5a0ddf5987fddf7d9484c46
END-i <evgeniy.donev@gmail.com>
2020-08-05 18:03:35 +0300

    fix

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -29,6 +29,7 @@ export default function ({ setShow }: TableProps) {
 
   useEffect(() => {
     handleSortList();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [sortBy]);
 
   const handleChange = (id: string, checked: boolean) => () => {
